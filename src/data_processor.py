5de6c7666cb9a4b4ffa5ed3318f5bbc55c98d07a
blacksheephandg-source <blacksheephandg@gmail.com>
2026-03-03 06:18:16 -0500

    Add data_processor.py with functions for removing vig, calculating true probabilities, identifying arbitrage opportunities, and processing live odds data

--- src/data_processor.py
+++ src/data_processor.py
@@ -1,46 +1,34 @@
-import pandas as pd
+def remove_vig(odds):
+    """
+    Removes the vig from the given odds.
+    :param odds: Dictionary with 'decimal' or 'fractional' odds.
+    :return: Odds without vig.
+    """
+    # Implementation here
 
 
-def american_to_decimal(american_odds):
-    if american_odds > 0:
-        return (american_odds / 100) + 1
-    else:
-        return (100 / abs(american_odds)) + 1
+def calculate_true_probabilities(odds):
+    """
+    Calculate true probabilities from given odds.
+    :param odds: Dictionary with 'decimal' or 'fractional' odds.
+    :return: True probabilities.
+    """
+    # Implementation here
 
 
-def calculate_probability(odds):
-    return 1 / odds
+def identify_arbitrage_opportunities(odds):
+    """
+    Identify arbitrage opportunities from a list of odds.
+    :param odds: List of odds from different bookmakers.
+    :return: List of arbitrage opportunities.
+    """
+    # Implementation here
 
 
-def vig_removal(odds, market_type='2-way'):
-    if market_type == '2-way':
-        decimal_odds = [american_to_decimal(odds[0]), american_to_decimal(odds[1])]
-        implied_prob = [calculate_probability(odd) for odd in decimal_odds]
-        total_prob = sum(implied_prob)
-        return [prob / total_prob for prob in implied_prob]
-    elif market_type == '3-way':
-        decimal_odds = [american_to_decimal(odd) for odd in odds]
-        implied_prob = [calculate_probability(odd) for odd in decimal_odds]
-        total_prob = sum(implied_prob)
-        return [prob / total_prob for prob in implied_prob]
-
-
-def parse_odds_event(event_data):
-    event_info = {
-        'event': event_data.get('event_name'),
-        'date': event_data.get('event_date'),
-        'odds': event_data.get('odds')
-    }
-    return event_info
-
-
-def identify_arbitrage(opportunities):
-    arbitrage_opps = []
-    for opportunity in opportunities:
-        if opportunity['probability'] < 1:
-            arbitrage_opps.append(opportunity)
-    return arbitrage_opps
-
-
-def convert_to_dataframe(data):
-    return pd.DataFrame(data)
+def process_live_odds_data(data):
+    """
+    Process live odds data from various sources.
+    :param data: Raw live odds data.
+    :return: Processed odds data.
+    """
+    # Implementation here
